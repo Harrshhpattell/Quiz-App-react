2dd2715f019004b6e8d79b5fce8d4e0ee2186b83
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-10 00:41:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
 
 function App() {
+  const initialState = {
+    questions: [],
+
+    status: "Loading...",
+  };
+
+  function reducer(state, action) {}
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
   return (
     <div className="app">
       <Header />
@@ -14,3 +31,5 @@ function App() {
 }
 
 export default App;
+
+// npm i json-server
