62b6044eec27dbbfd636680967df05539acfd230
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-11 01:16:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,29 +2,51 @@ import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import { useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "Loading...",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
-  const initialState = {
-    questions: [],
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
-    status: "Loading...",
-  };
+  const numQuestions = questions.length;
 
-  function reducer(state, action) {}
-
-  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading..." && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
